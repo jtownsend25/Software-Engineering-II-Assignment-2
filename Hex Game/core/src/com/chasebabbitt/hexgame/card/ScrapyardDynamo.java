0bbfab0249b3479bf4423b4baa87b55cfdf1fa8c
jtownsend25 <jtownsend25@users.noreply.github.com>
2017-02-03 15:17:16 -0800

    Create ScrapyardDynamo.java

---
+++ Hex Game/core/src/com/chasebabbitt/hexgame/card/ScrapyardDynamo.java
@@ -0,0 +1,13 @@
+package com.chasebabbitt.hexgame.card;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+
+public class com.chasebabbitt.hexgame.card.ScrapyardDynamo extends Card{
+	name = "Scrapyard Dynamo";
+	attackpoints = 1;
+	defensepoints = 1;
+	cost = 1;
+	//vanilla
+}
